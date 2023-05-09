f65527ba9e99e70c98284fb74e5986f04446ee86
Irina Pospelova <irapospelova78@gmail.com>
2023-05-09 17:09:23 +0300

    Test passed

--- src/test/java/ru/netology/rest/PostmanTest.java
+++ src/test/java/ru/netology/rest/PostmanTest.java
@@ -19,7 +19,7 @@ public class PostmanTest {
 
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Victory Day"))
+                .body("data", equalTo("Today's Victory Day"))
         ;
     }
 
