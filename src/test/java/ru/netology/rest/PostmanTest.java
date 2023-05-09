a3c8e56a03bea1cf3582886d0510133646e8d80f
Irina Pospelova <irapospelova78@gmail.com>
2023-05-09 16:53:30 +0300

    Test failed

---
+++ src/test/java/ru/netology/rest/PostmanTest.java
@@ -0,0 +1,26 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.*;
+public class PostmanTest {
+    @Test
+            void shouldReturnCorrectData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Today's Victory Day") // отправляемые данные (заголовки и query можно выставлять аналогично)
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Victory Day"))
+        ;
+    }
+
+}
